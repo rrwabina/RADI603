d1891c08fa6f4c240a3c974b5b6d5f3b80e6bfa5
Romen Samuel Wabina <romensamuel@gmail.com>
2022-09-20 04:47:23 +0800

    add files

---
+++ SQL/Practice.sql
@@ -0,0 +1,29 @@
+-- SELECT * FROM CUSTOMERS;
+
+
+# SQL Inner Join Example
+-- SELECT Orders.OrderID, Customers.CustomerName, Shippers.ShipperName
+-- FROM ((Orders
+-- INNER JOIN Customers ON Orders.CustomerID = Customers.CustomerID)
+-- INNER JOIN Shippers ON Orders.ShipperID = Shippers.ShipperID);
+
+# SQL Group By
+-- SELECT Country, COUNT(CustomerID)
+-- FROM Customers
+-- GROUP BY Country
+-- ORDER BY COUNT(CustomerID) DESC;
+
+
+-- SELECT * FROM suppliers;
+
+-- SELECT Orders.OrderID, Customers.CustomerName, Shippers.ShipperName
+-- FROM ((Orders
+-- INNER JOIN Customers ON Orders.CustomerID = Customers.CustomerID)
+-- INNER JOIN Shippers ON Orders.ShipperID = Shippers.ShipperID);
+
+
+SELECT A.CustomerName AS CustomerName1, B.CustomerName AS CustomerName2, A.City
+FROM Customers A, Customers B
+WHERE A.CustomerID <> B.CustomerID
+AND A.City = B.City
+ORDER BY A.City;
