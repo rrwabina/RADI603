493d6125d5172d96e39dcd53967991303bb52b7a
Romen Samuel Wabina <romensamuel@gmail.com>
2022-09-28 17:15:24 +0800

    modified contents

--- Assignments/main.py
+++ Assignments/main.py
@@ -6,29 +6,29 @@ import pandera as pa
 from pandera.typing import DataFrame
 
 app = FastAPI()
-ref = pd.read_csv('ref.csv')
+ref = pd.read_csv("ref.csv")
 
-@app.get("/", response_class = HTMLResponse)
+@app.get("/", response_class=HTMLResponse)
 def root():
     return """
             <html>
                 <head>
-                    <title> HTML Elements Reference </title>
+                    <title>HTML Elements Reference</title>
                 </head>
                 <body>
-                    <h1> Rama ECG Project </h1>
-                    <p> This is the initial version. </p>
+                    <h1>Rama ECG Project</h1>
+                    <p>This is the initial version.</p>
                 </body>
             </html>
-          """
+            """
 
-@app.get('/infer/{text}')
+@app.post('/infer/{text}')
 def infer(text: str):
-    res = ref[ref['input'] == text]
+    res = ref[ref["input"] == text]
     if len(res.index) == 0:
         return "Unknown"
     else:
-        return res['output'].values[0]
+        return res["output"].values[0]
 
 if __name__ == "__main__":
-    uvicorn.run(app, host = '0.0.0.0', port = 80, debug = True)
+    uvicorn.run(app, host="0.0.0.0", port = 80, debug = True)
