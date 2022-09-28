5d80b239ff760d19c59f02150be2516f059c2d9e
Romen Samuel Wabina <romensamuel@gmail.com>
2022-09-28 16:37:38 +0800

    add files

---
+++ Assignments/main.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import uvicorn
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+import pandera as pa 
+from pandera.typing import DataFrame
+
+app = FastAPI()
+ref = pd.read_csv('ref.csv')
+
+@app.get("/", response_class = HTMLResponse)
+def root():
+    return """
+            <html>
+                <head>
+                    <title> HTML Elements Reference </title>
+                </head>
+                <body>
+                    <h1> Rama ECG Project </h1>
+                    <p> This is the initial version. </p>
+                </body>
+            </html>
+          """
+
+@app.get('/infer/{text}')
+def infer(text: str):
+    res = ref[ref['input'] == text]
+    if len(res.index) == 0:
+        return "Unknown"
+    else:
+        return res['output'].values[0]
+
+if __name__ == "__main__":
+    uvicorn.run(app, host = '0.0.0.0', port = 80, debug = True)
