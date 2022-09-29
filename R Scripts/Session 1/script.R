5e61b6902b3407eefbfde65522a6640359cd0998
Romen Samuel Wabina <romensamuel@gmail.com>
2022-09-29 13:04:21 +0800

    added files

--- R Scripts/Session 1/script.R
+++ R Scripts/Session 1/script.R
@@ -78,8 +78,6 @@ carReg <- lm(speed~dist, data=dataCar)
 aa <- emp.data[, 1:2]
 bb <- emp.data[1:2, ]
 
-
-
 # Statistical Functions in R
 # Descriptive statistics --> summary(), stem()
 
@@ -87,4 +85,57 @@ irisData <- iris
 summary(irisData)
 plot(irisData$Sepal.Length, irisData$Sepal.Width)
 print(table(iris$Species))
+
+dataCar = cars
+carReg <- lm(speed~dist, data=dataCar)
+summary(carReg)
+coef(carReg)
+
+hist(dataCar$speed, prob = T)
+lines(density(dataCar$speed))
+
+
+library(foreign)
+library(readxl)
+colonData <- read.table('../data/colon.csv', header = TRUE, sep = ',')
+colonExcelData <- read_excel('../data/colon.xlsx', sheet = 'colon')
+colonData$Class <- factor(colonData$Class)
+colonData$Class <-as.numeric(colonData$Class)
+
+write.csv(colonData,'../data/colonTestSave.csv', row.names = FALSE)
+library(writexl)
+write_xlsx(list(conlonSheet = colonData),"../data/colonTestSaveExcel.xlsx")
+write_xlsx(list(irisSheet = iris, carsSheet = cars, mtcarsSheet = mtcars), "../data/mydata.xlsx")
+
+
+myPlot <- data.frame(
+  x1 = c(175,166,181,168,177),
+  x2 = c(65.5,45.3,95.01,65,80),
+  stringsAsFactors = FALSE
+)
+plot(myPlot)
+
+
+hist(colonData$H64808, prob=T)
+lines(density(colonData$H64807))
+
+
+categories <- table(colonData$Class)
+barplot(categories, col=c('red','green'))
+
+boxplot(H64807~Class, data = colonData)
+
+library(ggplot2)
+p <- ggplot(colonData, aes(colonData$H64807, colonData$T62947)) + 
+     geom_point(aes(colour = factor(colonData$Class)))
+p + scale_colour_manual(values = c("red", "green"))
+
+library(ggplot2)
+library(GGally)
+ggpairs(iris, title = "Scatterplot Matrix of the Features of the Iris Data Set")
+
+
+# Missing completely at random - the presence of missing values would not depend on the characteristics of the patient
+# Missing at random - depends on the characteristics of the observable data
+
 
