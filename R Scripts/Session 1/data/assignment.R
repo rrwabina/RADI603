a0b790a41f80186ec09f54a00b1d0f672c9d2238
rrwabina <romensamuel@gmail.com>
2022-10-11 20:38:12 +0800

    add files

--- R Scripts/Session 1/data/assignment.R
+++ R Scripts/Session 1/data/assignment.R
@@ -11,6 +11,9 @@ library(magick)
 library(tensorflow)
 library(keras)
 library(neuralnet)
+library(spatstat)
+library(OpenImageR)
+library(raster)
 
 setwd('C:/Users/Renan/Desktop/PhD Data Science/RADI 603/R Scripts/Session 1/data')
 df_autism <- read.csv('../data/Autism-Adolescent-Data.csv')
@@ -86,10 +89,10 @@ summary(tune)
 
 model <- svm(class~., data = autism_train, 
                       method = 'C-classification', 
-                      kernel = 'linear', 
+                      kernel = 'polynomial', degree = 3, 
                       probability = T, 
                       gamma = 1e-02, 
-                      cost = 100)
+                      cost = 1000)
 
 prediction <- predict(model, autism_test, probability = T)
 table(autism_test$class, prediction)
@@ -105,7 +108,7 @@ train_input  <- as.matrix(autism_train[, -16])
 train_output <- as.matrix(autism_train[,  17])
 test_input   <- as.matrix(autism_test[,  -16])
 
-prediction <-knn(train_input, test_input, train_output, k = 3)
+prediction <-knn(train_input, test_input, train_output, k = 4)
 xtab <- table(factor(prediction, levels = c(0, 1)), factor(autism_test$class, levels = c(0, 1)))
 results <- confusionMatrix(xtab)
 results
@@ -124,11 +127,12 @@ nnet_autismtrain <- cbind(nnet_autismtrain, autism_train$class == 0)
 nnet_autismtrain <- cbind(nnet_autismtrain, autism_train$class == 1)
 names(nnet_autismtrain)[18:19] <- c('NO', 'YES')
 
+cor(df_autism[, 1:16], as.numeric(df_autism[, 17]), method=c('pearson', 'kendall', 'spearman'))
+
 nn <- neuralnet(NO + YES ~ 
-                A1.Score + A2.Score + A3.Score + A4.Score + A5.Score  +
-                A6.Score + A7.Score + A8.Score + A9.Score + A10.Score +
-                age + gender + jaundice + autism + used,
-                data = nnet_autismtrain, hidden=c(5))
+                A3.Score + A4.Score + A5.Score + A6.Score  + 
+                A7.Score + A8.Score + A9.Score + A10.Score + used,
+                data = nnet_autismtrain, hidden=c(4))
 plot(nn)
 
 mypredict <- compute(nn, autism_test[-16])$net.result
@@ -162,21 +166,21 @@ for (idx in 1:(ncol(df_leukem)-1))
   df_leukem[, idx] <- as.numeric(df_leukem[, idx])
 }
 df_leukem[, 1:(ncol(df_leukem)-1)] <- scale(df_leukem[, 1:(ncol(df_leukem)-1)])
-
 #### CLASSIFIER: SVM Multi-Classification
 set.seed(224599)
-idx <- sample(2, nrow(df_leukem), replace = TRUE, prob=c(0.8, 0.2))
+idx <- sample(2, nrow(df_leukem), replace = TRUE, prob=c(0.7, 0.3))
 leukem_train <- as.data.frame(df_leukem[idx==1,])
 leukem_test  <- as.data.frame(df_leukem[idx==2,])
 tune  <- tune.svm(Class~., data = leukem_train, gamma = 10^(-10:-1), cost = 10^(1:5), tunecontrol = tune.control(cross = 10))
 summary(tune)
 
+help(svm)
 model <- svm(Class~., data = leukem_train, 
              method = 'C-classification', 
-             kernel = 'linear', 
+             kernel = 'polynomial', degree = 3,
              probability = T, 
-             gamma = 1e-02, 
-             cost = 1000)
+             gamma = 1e-05, 
+             cost = 1e+05)
 
 prediction <- predict(model, leukem_test, probability = T)
 table(leukem_test$Class, prediction)
@@ -190,7 +194,8 @@ train_input  <- as.matrix(leukem_train[, -15])
 train_output <- as.matrix(leukem_train[,  15])
 test_input   <- as.matrix(leukem_test[,  -15])
 
-prediction <-knn(train_input, test_input, train_output, k = 5)
+help(knn)
+prediction <- knn(train_input, test_input, train_output, k = 5)
 xtab <- table(prediction, leukem_test$Class)
 results <- confusionMatrix(xtab)
 results
@@ -199,7 +204,7 @@ as.matrix(results, what = "classes")
 
 #################################################### LEUKEMIA DATA ANN ###########################################################
 set.seed(152)
-idx <- sample(2, nrow(df_leukem), replace = TRUE, prob=c(0.7, 0.3))
+idx <- sample(2, nrow(df_leukem), replace = TRUE, prob=c(0.8, 0.2))
 leukem_train <- as.data.frame(df_leukem[idx==1,])
 leukem_test  <- as.data.frame(df_leukem[idx==2,])
 nnet_leukemtrain <- leukem_train
@@ -209,11 +214,13 @@ nnet_leukemtrain <- cbind(nnet_leukemtrain, leukem_train$Class == 1)
 nnet_leukemtrain <- cbind(nnet_leukemtrain, leukem_train$Class == 2)
 names(nnet_leukemtrain)[16:18] <- c('ALL', 'MLL', 'AML')
 
+cor(df_leukem[, 1:14], as.numeric(df_leukem[, 15]), method=c('pearson', 'kendall', 'spearman'))
+
 nn <- neuralnet(ALL + MLL + AML ~ 
-                g1 + g2 + g3  + g4  + g5  + g6  + g7 +
-                g8 + g9 + g10 + g11 + g12 + g13,
-                data = nnet_leukemtrain, hidden=c(2))
-plot(nn)
+                g4  + g5  + g7 + g8 + g10 + g12,
+                rep = 100, data = nnet_leukemtrain, hidden=c(3),
+                learningrate = NULL, act.fct = 'softplus')
+help(neuralnet)
 mypredict <- compute(nn, leukem_test[-14])$net.result
 maxidx <- function(arr){ 
   return(which(arr == max(arr)))
@@ -250,13 +257,28 @@ xray1 <- image_read('../data/datatwo/x-ray01.jpg', density = NULL, depth = NULL,
 image_info(xray1)
 image_convert(xray1, format = 'png', depth = NULL)
 
-image_border(image_background(xray1, 'hotpink'), "#000350", '10x10')
+
 image_trim(xray1)
-image_crop(xray1,  '150x150+20')
-image_scale(xray1, 'x500') 
+image_crop(xray1,  '100x100')
+
+image_scale(xray1, 'x500')
+image_scale(xray1, 'x100')
+image_scale(xray1, 'x50')
+
+image_rotate(xray1, 45)
 image_rotate(xray1, 90)
+
+image_modulate(xray1, brightness = 20, saturation = 120, hue = 90)
 image_modulate(xray1, brightness = 50, saturation = 120, hue = 90)
+
+my_raster <- as.raster(xray1) 
+par(mar = c(0, 0, 0, 0))
+plot(my_raster[1:125, 1:102])
+
+
 image_fill(xray1, 'blue', point = '+100+200', fuzz = 30)
+
+image_border(image_background(xray1, 'hotpink'), "#000350", '10x10')
 image_annotate(
   xray1, text = 'X-ray 1', size = 30, color = "blue",
   gravity = 'center')
@@ -265,3 +287,25 @@ image_annotate(
   color = 'red', boxcolor = 'white',
   degrees = 0, location = '+50+100',
   font = 'Arial')
+
+
+datagen <- image_data_generator(
+  rescale = 1/255,
+  rotation_range = 40,
+  width_shift_range = 0.2,
+  height_shift_range = 0.2,
+  shear_range = 0.2,
+  zoom_range = 0.2,
+  horizontal_flip = TRUE,
+  fill_mode = "nearest"
+)
+xray1 <- image_read('../data/datatwo/x-ray01.jpg', density = NULL, depth = NULL, strip = FALSE)
+
+img_path = '../data/datatwo/x-ray01.jpg'
+image1 = readImage(img_path) 
+
+augmentation_generator <- flow_images_from_data(
+  image1, 
+  generator = datagen,
+  batch_size = 1
+)
